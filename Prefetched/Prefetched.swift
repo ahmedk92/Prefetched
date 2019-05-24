64f82a89a8626ad07b019c91c821ac0cd109e9a3
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2019-05-24 22:42:29 +0200

    basic demo

---
+++ Prefetched/Prefetched.swift
@@ -0,0 +1,56 @@
+//
+//  Prefetched.swift
+//  Prefetched
+//
+//  Created by Ahmed Khalaf on 5/24/19.
+//  Copyright © 2019 Ahmed Khalaf. All rights reserved.
+//
+
+import Foundation
+
+class Prefetched<T> {
+    let queue: DispatchQueue
+    let generator: () -> T
+    var status = Status.fresh
+    private var _value: T?
+    
+    init(queue: DispatchQueue, generator: @escaping () -> T) {
+        self.queue = queue
+        self.generator = generator
+    }
+    
+    var value: T {
+        switch status {
+        case .fresh:
+            print("Value requested when fresh")
+            prefetch()
+            queue.sync {}
+            return self.value
+        case .prefetching:
+            print("Value requested when prefetching")
+            queue.sync {}
+            return self.value
+        case .prefetched(let value):
+            return value
+        }
+    }
+    
+    func prefetch() {
+        guard case Status.fresh = status else { return }
+        
+        status = .prefetching
+        
+        queue.async { [weak self] in
+            guard let self = self else { return }
+            let value = self.generator()
+            self._value = value
+            self.status = .prefetched(value)
+        }
+    }
+}
+
+extension Prefetched {
+    enum Status {
+        case fresh, prefetching, prefetched(T)
+    }
+}
--- Prefetched/ViewController.swift
+++ Prefetched/ViewController.swift
@@ -8,13 +8,65 @@
 
 import UIKit
 
+class Cell: UITableViewCell {
+    @IBOutlet var label: UILabel!
+}
+
 class ViewController: UIViewController {
+    private let queue = DispatchQueue(label: "dataQueue")
+    
+    private lazy var data: [Prefetched<String>] = (0..<1000).map({ _ in
+        return Prefetched(queue: queue, generator: { () -> String in
+            Thread.sleep(forTimeInterval: 0.01) // Simulate heavy work e.g. db access, etc...
+            return randomText(length: Int.random(in: 32...256))
+        })
+    })
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension ViewController: UITableViewDataSource, UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! Cell
+        cell.label.text = data[indexPath.row].value
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            data[indexPath.row].prefetch()
+        }
+    }
+}
 
-
+// Credits: https://gist.github.com/skreutzberger/eac3edc7918d0251f366
+func randomText(length: Int, justLowerCase: Bool = false) -> String {
+    var text = ""
+    for _ in 1...length {
+        var decValue = 0  // ascii decimal value of a character
+        var charType = 3  // default is lowercase
+        if justLowerCase == false {
+            // randomize the character type
+            charType =  Int(arc4random_uniform(4))
+        }
+        switch charType {
+        case 1:  // digit: random Int between 48 and 57
+            decValue = Int(arc4random_uniform(10)) + 48
+        case 2:  // uppercase letter
+            decValue = Int(arc4random_uniform(26)) + 65
+        case 3:  // lowercase letter
+            decValue = Int(arc4random_uniform(26)) + 97
+        default:  // space character
+            decValue = 32
+        }
+        // get ASCII character from random decimal value
+        let char = String(UnicodeScalar(decValue)!)
+        text = text + char
+        // remove double spaces
+        text = text.replacingOccurrences(of: "  ", with: " ")
+    }
+    return text
 }
 
